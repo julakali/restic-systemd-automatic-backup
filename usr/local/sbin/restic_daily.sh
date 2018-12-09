0e9d5592d71886f839be5c3cbef7bd334615c573
root <root@restic.julakali.org>
2018-12-09 18:36:03 +0100

    revert to one timer + service, check day in bash script for weekly backups

--- usr/local/sbin/restic_daily.sh
+++ usr/local/sbin/restic_daily.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-/usr/local/sbin/restic_backup.sh --tag data /mnt/nfs/data/
-#/usr/local/sbin/restic_backup.sh --tag owncloud --prune /mnt/nfs/owncloud
+/usr/local/sbin/restic_backup.sh --tag data --exclude .snapshots /mnt/nfs/data/
+/usr/local/sbin/restic_backup.sh --tag owncloud --exclude .snapshots /mnt/nfs/owncloud
+
+if [[ $(date +%u) -eq 6 ]]; then
+	# perform weekly backups on saturday
+	/usr/local/sbin/restic_backup.sh --tag Musik --exclude .snapshots /mnt/nfs/Musik
+	/usr/local/sbin/restic_backup.sh --tag Fotos --exclude .snapshots --prune /mnt/nfs/Fotos
+fi
